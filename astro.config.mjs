f1481d59175fbdde3918a7b6409f5ef624674602
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-04-14 19:18:05 +0300

    new flaco

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import devtoolBreakpoints from "astro-devtool-breakpoints";
+
+// https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: 'css-variables' ,
+      theme: 'css-variables',
       wrap: true,
       skipInline: false,
       drafts: false
     }
   },
   site: 'https://lexingtonthemes.com',
-  integrations: [preact(), tailwind(), sitemap()]
+  integrations: [preact(), tailwind(), sitemap(), devtoolBreakpoints()]
 });
